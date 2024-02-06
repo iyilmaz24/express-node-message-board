ee07284e1cad0609e57e1e85a96d1f708719c92b
Irfan Yilmaz <iy.usa@outlook.com>
2024-02-06 18:21:49 -0500

    change 'var' to 'const', create display template and messages array
    change all occurences of var to const in users.js, index.js and app.js
    create display.jade template for use in displaying messages
    create messages array in index.js to hold messages to pass as argument
    use messages array in /display res.render argument to populate jade template

--- message-board/routes/index.js
+++ message-board/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+const messages = [
+  {
+    text: "Hello there!",
+    user: "Jane",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/display', function(req, res, next) {
+  res.render('display', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+
+
+
+});
+
 
 module.exports = router;
