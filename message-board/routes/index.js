af92ae1dc53e3f27a1f0beb9d2c546b0393c650b
Irfan Yilmaz <iy.usa@outlook.com>
2024-02-06 18:41:45 -0500

    create form.jade, allow user to add new message
    create form.jade template to allow addition of user message to board
    after submission of user form input, push information to messages array
    redirect user to /display route and allow them to see their message

--- message-board/routes/index.js
+++ message-board/routes/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-
 const messages = [
   {
     text: "Hello there!",
     user: "Jane",
-    added: new Date()
+    added: new Date().toDateString()
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date().toDateString()
   }
 ]
 
@@ -20,14 +19,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// displaying all messages
 router.get('/display', function(req, res, next) {
   res.render('display', { title: 'Mini Message Board', messages: messages });
 });
 
+
+// creating a new message
 router.get('/new', function(req, res, next) {
-
-
-
+  res.render('form')
+});
+router.post('/new', function(req, res, next) {
+  messages.push({text:req.body.messageI, user:req.body.nameI, added: new Date().toDateString()})
+  res.redirect('/display')
 });
 
 
